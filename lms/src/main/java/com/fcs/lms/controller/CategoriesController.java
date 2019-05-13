fd8d52ee1fd2a877e8b31187164c15d4b701b9c9
congdoan176 <nguyencongdoan268@gmail.com>
2019-05-13 17:25:45 +0700

    them teamplate trang admin - hien thi ra dc danh sach category, add category.

--- lms/src/main/java/com/fcs/lms/controller/CategoriesController.java
+++ lms/src/main/java/com/fcs/lms/controller/CategoriesController.java
@@ -1,23 +1,27 @@
 package com.fcs.lms.controller;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import com.fcs.lms.entity.Category;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.fcs.lms.entity.Category;
-import com.fcs.lms.entity.Course;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.Firestore;
@@ -46,28 +50,29 @@ public class CategoriesController {
 	}
 	
 	// tao moi category
-	@PostMapping(value = "/c/create")
-	public String addCategory(@Valid Category category, String name, String url, Model model) {
-		
+	@RequestMapping(value = "/c/create",method = RequestMethod.POST)
+	public void addCategory(@Valid Category category, String name, String url, Model model, HttpServletResponse resp) throws IOException{
 		Firestore db = FirestoreOptions.getDefaultInstance().getService();
 		Map<String, Object> categories = new HashMap<>();
 		categories.put("name", name);
 		categories.put("url", url);
 		ApiFuture<DocumentReference> addedDocRef = db.collection("categories").add(categories);
-		
 		LOGGER.info("Show Category :"+ categories);
-		
- 		return "views/category/index";
+		resp.sendRedirect("/c");
 	}
 	
 	@GetMapping(value = "/c/create")
 	public String formCreat() {
 		return "views/category/create";
 	}
-	
+	@GetMapping(value = "c/edit/{url}")
+	public String showUpdateForm(@PathVariable String name) {
+		
+		return "update-category";
+	}
 	@PutMapping(value = "/c/edit")
 	public String updateCategory() {
-		return null;
+		return "views/category/edit";
 	}
 	@GetMapping(value = "/c/delete/{id}")
 	public String deleteCategory() {
